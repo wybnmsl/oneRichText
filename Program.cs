eb402ed8a0a6b69e51bde9428130de6e9683abb9
835670119@qq.com <835670119@qq.com>
2020-06-10 21:15:09 +0800

    第二版 添加RichTextBox

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace 一个
+namespace DemoRichText
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace 一个
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
