706ba95955c511e106df3f101b0e2574c9351136
835670119@qq.com <835670119@qq.com>
2020-06-10 21:21:08 +0800

    第五版 前端功能对接

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace 一个
+namespace origin
 {
     static class Program
     {
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace 一个.Properties
+namespace origin.Properties
 {
 
 
